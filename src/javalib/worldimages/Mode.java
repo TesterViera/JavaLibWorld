22525ef9b5ae10b3d8ad4557774db3262ce50daf
Stephen Bloch <sbloch@adelphi.edu>
2013-02-10 20:57:28 -0500

    Gave the Mode class a couple of aliases: SOLID, solid, FILLED, and filled all mean the same thing, while OUTLINED and outlined mean the same thing.

--- src/javalib/worldimages/Mode.java
+++ src/javalib/worldimages/Mode.java
@@ -2,12 +2,17 @@ package javalib.worldimages;
 
 
 /**
- * Enumeration class Mode - write a description of the enum class here
+ * The available Modes are FILLED (aka SOLID) and OUTLINED.
  * 
  * @author Stephen Bloch
- * @version Dec 24, 2012
+ * @version Feb 10, 2013
  */
 public enum Mode
 {
-    FILLED, OUTLINED
+    FILLED, OUTLINED;
+    
+    public static final Mode SOLID = FILLED;
+    public static final Mode filled = FILLED;
+    public static final Mode solid = FILLED;
+    public static final Mode outlined = OUTLINED;
 }
